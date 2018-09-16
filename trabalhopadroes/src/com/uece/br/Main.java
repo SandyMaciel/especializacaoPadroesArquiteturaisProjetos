89da6a21a506b0eb275309d61d42aa736af1d481
kartmatias <kartmatias@gmail.com>
2018-09-15 23:41:56 -0300

    Outros padroes

--- trabalhopadroes/src/com/uece/br/Main.java
+++ trabalhopadroes/src/com/uece/br/Main.java
@@ -13,9 +13,13 @@ import com.uece.br.factoryMethod.DecodedImage;
 import com.uece.br.factoryMethod.IVisualizador;
 import com.uece.br.factoryMethod.VisualizadorBmp;
 import com.uece.br.factoryMethod.VisualizadorJpg;
+import com.uece.br.observer.Alarme;
+import com.uece.br.observer.PoliciaObserver;
+import com.uece.br.observer.SeguroObserver;
 import com.uece.br.singleton.Logger;
 import com.uece.br.strategy.LoggerEventViewer;
 import com.uece.br.strategy.StrategyContext;
+import com.uece.br.templateMethod.*;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -31,35 +35,44 @@ public class Main {
 
         // 1. Singleton
         System.out.println(ConsoleColors.RED + "EXEMPLO SINGLETON" + ConsoleColors.RESET + " =");
-        Ex01Singleton();
+        RunSingleton();
 
         // 2. Strategy
         System.out.println(ConsoleColors.GREEN + "EXEMPLO STRATEGY" + ConsoleColors.RESET + " =");
-        Ex02Strategy();
+        RunStrategy();
 
         // 3. FactoryMethod
         System.out.println(ConsoleColors.YELLOW + "EXEMPLO FACTORY METHOD" + ConsoleColors.RESET + " =");
-        Ex03FactoryMethod("foto.bmp");
-        Ex03FactoryMethod("foto.jpg");
+        RunFactoryMethod("foto.bmp");
+        RunFactoryMethod("foto.jpg");
 
         // 4. Decorator
         System.out.println(ConsoleColors.BLUE + "EXEMPLO DECORATOR" + ConsoleColors.RESET + " =");
-        Ex04Decorator();
+        RunDecorator();
 
         // 5. Chain
         System.out.println(ConsoleColors.CYAN + "EXEMPLO CHAIN" + ConsoleColors.RESET + " =");
-        Ex05Chain();
+        RunChain();
 
         // 6. Command
+        RunCommand();
+
+        // 7. Observer
+        System.out.println(ConsoleColors.YELLOW_BRIGHT + "EXEMPLO OBSERVER" + ConsoleColors.RESET + " =");
+        RunObserver();
+
+        // 8. Template Method
+        System.out.println(ConsoleColors.RED_BOLD + "EXEMPLO TEMPLATE METHOD" + ConsoleColors.RESET + " =");
+        RunTemplateMethod();
 
 
         //11. Abstract Factory
         System.out.println(ConsoleColors.PURPLE + "EXEMPLO ABSTRACT FACTORY" + ConsoleColors.RESET + " =");
-        Ex11AbstractFactory();
+        RunAbstractFactory();
 
     }
 
-    private static void Ex01Singleton(){
+    private static void RunSingleton(){
         Logger logger1 = Logger.getInstance();
         logger1.setAtivo(true);
         logger1.log("Primeira mensagem");
@@ -69,7 +82,7 @@ public class Main {
 
     }
 
-    public static void Ex02Strategy(){
+    public static void RunStrategy(){
         StrategyContext ctx = new StrategyContext();
 
         ctx.setLoggerStrategy(new LoggerEventViewer());
@@ -77,7 +90,7 @@ public class Main {
         ctx.CreateLog("Usando EVENTVIEWER");
     }
 
-    public static void Ex03FactoryMethod(String image){
+    public static void RunFactoryMethod(String image){
         DecodedImage decodedImage;
         IVisualizador visualizador = null;
         String formato = image.substring(image.indexOf('.')+1, image.length());
@@ -92,7 +105,7 @@ public class Main {
         System.out.println(decodedImage);
     }
 
-    public static void Ex04Decorator(){
+    public static void RunDecorator(){
         IFrozenItem frozen = new FrozenBase("yogurte de frutas", 15);
         frozen = new Topo("castanha",8,frozen);
         frozen = new Topo("frutas cristalizadas",6,frozen);
@@ -102,7 +115,7 @@ public class Main {
 
     }
 
-    public static  void Ex05Chain(){
+    public static  void RunChain(){
         CedulaChain cedulas = new CedulaUm();
         cedulas.setNext(new CedulaCinco());
         cedulas.setNext(new CedulaDez());
@@ -118,7 +131,41 @@ public class Main {
 
     }
 
-    public static void Ex11AbstractFactory() throws ParseException {
+    public static void RunCommand(){
+        System.out.println("...");
+    }
+
+    public static void RunObserver(){
+        Alarme ast = new Alarme();
+
+        new PoliciaObserver(ast);
+        new SeguroObserver(ast);
+
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++){
+            System.out.println("\nSimule o evento");
+            ast.setState(scan.nextInt());
+        }
+
+    }
+
+    public static void RunTemplateMethod(){
+        TransformaStringTemplate mai = new TransformaMaiuscula();
+        mai.imprimeString();
+
+        TransformaStringTemplate min = new TransformaMinuscula();
+        min.imprimeString();
+
+
+        TransformaStringTemplate inverte = new TransformaInverte();
+        inverte.imprimeString();
+
+        TransformaStringTemplate duplica = new TransformaDuplicacao();
+        duplica.imprimeString();
+    }
+
+    public static void RunAbstractFactory() throws ParseException {
         Scanner ler = new Scanner(System.in);
         System.out.println("Entre com uma data: ");
         String dtRecebida = ler.next();
